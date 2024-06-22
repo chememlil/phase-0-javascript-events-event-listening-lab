5595e22b7e5969510364b72143c0f012cb067d0f
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-06-22 12:17:43 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener(); // Call the function to set up the event listener
